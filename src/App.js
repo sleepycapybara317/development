2aa205fe7d450eddad48c27e28a4bf111549c3a1
sleepycapybara317 <isha_ponugoti@brown.edu>
2022-12-04 22:54:37 -0500

    sort first try

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import "./App.css";
-import { useState } from "react";
+// import { useState } from "react";
 import drinkData from "./assets/drink-data.json";
 import DrinkItem from "./components/DrinkItem";
+import React, { useState } from 'react';
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 drinkData.forEach((item) => {
   item.image = process.env.PUBLIC_URL + "/" + item.image;
-  console.log(item.image)
 });
 /* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
   const [totalPrice, setTotalPrice] = useState(0);
   const [totalQuantity, setTotalQuantity] = useState(0);
   const [cart, setCart] = useState([]);
@@ -45,25 +43,59 @@ function App() {
 
     addPrice(price);
     addQuantity();
-  }
+  };
 
+  // make list of drink components
+  const allDrinks = drinkData.map((item) => {
+    return <DrinkItem key = {item.id} name = {item.name} image = {item.image} desc = {item.description} cal = {item.calories} temp = {item.temp} price = {item.price} addToCart = {addItemToCart}/>;
+  });
+
+  const [drinks, setDrinks] = useState(allDrinks);
+  const sortPrices = () => {
+    const sorted = [...drinks].sort((a,b) => {
+      if (a.price > b.price) return 1;
+      if (a.price < b.price) return -1;
+      return 0;
+    });
+    setDrinks(sorted)
+  }
   return (
     <div className="App">
-      <h1>Queen Bean Café</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>Queen Bean Café</h1> {}
 
-      {drinkData.map((item) => ( // TODO: map drinkData to DrinkItem components
+      {/* {drinkData.map((item) => ( // TODO: map drinkData to DrinkItem components
         <p>
           <DrinkItem name = {item.name} image = {item.image} desc = {item.description} price = {item.price} addToCart = {addItemToCart}/>
-        </p> // replace with DrinkItem component
-      ))}
+        </p> 
+      ))} */}
 
-    <div className="cart-container">
-        <h2>My Cart ({totalQuantity})</h2>
-        {cart.map((item) => (
-        <p>
-          {item.quantity}x {item.name}
-        </p> ))}
-        <h3>Subtotal: ${totalPrice.toFixed(2)}</h3>
+    {/* print allDrinks */}
+    {drinks.map((item) => ( // TODO: map drinkData to DrinkItem components
+        <div>
+          {item};
+        </div> 
+    ))}
+      <div className="filter-container">
+        <button onClick={sortPrices}>
+        SORT By Price (low - high)
+        </button>
+        <h3>Beverage Type</h3>
+        <h4>Coffee</h4>
+        <h4>Tea</h4>
+        <h4>Other</h4>
+        <h4>All</h4>
+        <h3>Temperature</h3>
+        <h4>Hot</h4>
+        <h4>Cold</h4>
+      </div>
+
+      <div className="cart-container">
+          <h2>My Cart ({totalQuantity})</h2>
+          {cart.map((item) => (
+          <p>
+            {item.quantity}x {item.name}
+          </p> ))}
+          <h3>Subtotal: ${totalPrice.toFixed(2)}</h3>
       </div>
     </div>
   );
